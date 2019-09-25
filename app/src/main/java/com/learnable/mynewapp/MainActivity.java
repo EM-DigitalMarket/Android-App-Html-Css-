713e385d98a607763772cd7bb6d80313225b5e0d
Ebada magdy <ebadapo@yahoo.com>
2019-09-25 13:17:32 +0200

    home/categres

---
+++ app/src/main/java/com/learnable/mynewapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.learnable.mynewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView;
+        myWebView = (WebView) findViewById(R.id.web);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("file:///android_asset/index.html");
+    }
+}
